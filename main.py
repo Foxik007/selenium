0382b1c8ddb82d98962112ebd62a97c958e2b644
mr.Django <textile93@mail.ru>
2022-03-23 09:51:22 +0300

    add selenium

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+import time
 
-driver = webdriver.Chrome('chromedriver.exe')
+from selenium import webdriver
+from selenium.webdriver.common import keys
+from selenium.webdriver.common.actions.interaction import KEY
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome('Users\Денис\PycharmProjects\PythonTutorial\chromedriver.exe')
+driver.get('https://google.com/')
+driver.find_element(By.XPATH,'//*[@title=\'Поиск\']').send_keys('ютубчик' + Keys.RETURN)
+
+driver.save_screenshot('goog.png')
+
+time.sleep(3)
+
+driver.quit()
