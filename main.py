80c1b848b9de5cfd337410a1ff2c03f789f7271b
mr.Django <textile93@mail.ru>
2022-03-26 09:43:15 +0300

    sleep5

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome('Users\Денис\PycharmProjects\PythonTutorial\chromedriver.exe')
 driver.get('http://130.193.37.179/app/pets')
 (driver.find_elements(By.XPATH,'//*[@id="image"]/img'))[0].click()
-time.sleep(4)
+time.sleep(5)
 driver.quit()
